6f824f89b33ac69a3407d5abc8834fbefa4241cf
volodymyrZotov <volodymyr.zotov@gmail.com>
2017-10-05 14:01:22 +0300

    added pagination scrapper

---
+++ pagination_scrapper.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class TestSpider(scrapy.Spider):
+    name = 'test'
+    allowed_domains = ['http://quotes.toscrape.com/']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        quotes = response.css('div.quote')
+        for quote in quotes:
+            yield {
+                'author': quote.css('small.author::text').extract_first(),
+                'text': quote.css('span.text::text').extract_first(),
+                'tags': quote.css('a.tag::text').extract()
+            }
+
+        next_page_url = response.css('li.next > a::attr(href)').extract_first()
+        if next_page_url:
+            next_page_url = response.urljoin(next_page_url)
+            yield scrapy.Request(url=next_page_url, callback=self.parse, dont_filter=True)
